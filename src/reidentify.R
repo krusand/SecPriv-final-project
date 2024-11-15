485d5f113cce4835a0e7525e7a6f68995cebb0a1
krusand <110490348+krusand@users.noreply.github.com>
2024-11-15 11:43:38 +0100

    Finished reidentification for check

--- src/reidentify.R
+++ src/reidentify.R
@@ -1,11 +1,13 @@
-# Setup
+# Setup ----
 setwd("/Users/krusand/Documents/GitHub/SecPriv-final-project/")
 library(tidyverse)
 library(openxlsx)
 
 dt_survey <- as.Date("2024-07-07")
 
+# Load data ----
 survey_list <- read.csv("data/leaked/survey_listC.txt", sep = ".")
+
 anonymized_data <- read.csv("data/anonymized/anonymised_dataC.csv") %>% 
   select(-education) %>% 
   mutate(zip = as.character(zip))
@@ -18,6 +20,8 @@ public_data_register_raw <- read.xlsx("data/leaked/public_data_registerC.xlsx") 
   filter(name %in% survey_list$name) %>% 
   select(-last_voted)
 
+# Wrangle data ----
+
 pub_eu_country <- c("Belgium", "France", "Germany", "Hungary", "Iceland","Italy","Portugal","Slovakia","Spain", "Sweden")
 pub_non_eu_country <- c("Afghanistan","China","Serbia", "Syria", "Thailand", "Turkey", "United Kingdom")
 age_labs = c("18-24", "25-29","30-39","40-49","50-59", '60-69', "70+")
@@ -26,7 +30,7 @@ public_data_register <- public_data_register_raw %>%
   mutate(age = year(as.period(interval(start=dob, end=dt_survey)))) %>% 
   mutate(age = cut(age, breaks = c(18, 24, 29, 39, 49, 59, 69, Inf), labels=age_labs)) %>% 
   mutate(marital_status = case_when(
-    marital_status == 'Married' ~ 'Yes',
+    marital_status == 'Married/separated' ~ 'Yes',
     T ~ 'No'
   )) %>% 
   mutate(citizenship = case_when(
@@ -36,24 +40,41 @@ public_data_register <- public_data_register_raw %>%
   )) %>% 
   select(name, sex,evote,age,zip,citizenship, marital_status)
 
+# Identify political preferences ----
 
-
-
-anonymized_data %>% 
+## Join all non na cols ----
+df_all <- anonymized_data %>% 
+  filter(!is.na(age) & !is.na(zip)) %>% 
   left_join(public_data_register, 
-            by = c("sex","evote","age","zip","citizenship", "marital_status")) %>% 
-  filter(is.na(name)) %>% 
+            by = c("sex", "evote", "age", "zip", "citizenship", "marital_status"))
+
+
+## Join on all other columns than age ----
+df_na_age <- anonymized_data %>% 
+  filter(is.na(age)) %>% 
+  left_join(public_data_register %>% select(-age), 
+            by = c("sex", "evote", "zip", "citizenship", "marital_status")) 
+
+## Join on all other columns than zip ----
+df_na_zip <- anonymized_data %>% 
+  filter(is.na(zip)) %>% 
+  left_join(public_data_register %>% select(-zip), 
+            by = c("sex", "evote", "age", "citizenship", "marital_status")) 
+
+
+
+# Bind and filter ----
+bind_rows(list(df_all, df_na_age, df_na_zip)) %>%
+  group_by(sex, evote, age, zip, citizenship, marital_status) %>% 
+  mutate(n=n()) %>% 
+  mutate(n_dist = n_distinct(party)) %>% 
+  ungroup() %>% 
+  arrange(across(everything())) %>%
+  filter(n_dist == 1) %>% 
+  filter(!is.na(name)) %>% 
+  distinct(name, party) %>% 
+  add_count(name) %>% 
+  filter(n == 1) %>% 
   View()
-
-
-
-public_data_register_raw %>% 
-  count(marital_status)
-
-
-
-
-
-
 
 
